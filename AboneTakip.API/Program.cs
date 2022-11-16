eed883fb8be64185bdbcdf5afd182e30582111f9
Berkay Özışık <bozisik93@hotmail.com>
2022-11-16 22:04:03 +0300

    FluentValidation problem solved.

--- AboneTakip.API/Program.cs
+++ AboneTakip.API/Program.cs
@@ -1,9 +1,12 @@
 using AboneTakip.API.Autofac;
 using AboneTakip.API.Extensions;
 using AboneTakip.Business.AutoMapper.Profiles;
+using AboneTakip.Business.EntityValidation.Concrete.Customers;
 using AboneTakip.DataAccess.EntitiyFramework.Context;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +14,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AboneTakipDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConStr")));
 
 
-builder.Services.AddCustomFluentValidation();
+builder.Services.Configure<ApiBehaviorOptions>(o =>
+{
+    o.SuppressModelStateInvalidFilter = true;
+});
 
 builder.Services.AddAutoMapper(typeof(IProfile).Assembly);
 
@@ -23,7 +29,7 @@ builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 builder.Host.ConfigureContainer<ContainerBuilder>(containerbuilder => containerbuilder.RegisterModule(new ServiceModuleExtension()));
 // AUTOFAC
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateCustomerValidator>());
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
